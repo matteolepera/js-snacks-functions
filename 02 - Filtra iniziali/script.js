2d95a392109268e78bd724e067ba812b302eedc1
matteolpera <matteo.lepera02@gmail.com>
2025-11-04 14:44:57 +0100

    second snack end

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const filterByFirstLetter = (array, letter) => {
 
+    const arrayByFirstLetter = [];
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i][0] === letter) {
+            arrayByFirstLetter.push(array[i]);
+        }
+
+    } return arrayByFirstLetter;
+}
+
+let insertLetter = prompt("Inserisci una lettera!");
+console.log(insertLetter);
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filterByFirstLetter(names, insertLetter));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
