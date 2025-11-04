dbd5ccf5260ac8cdd9bf6409da1e9749316df3b0
matteolpera <matteo.lepera02@gmail.com>
2025-11-04 15:07:19 +0100

    third snack end

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const counterVocal = (string) => {
+    string = string.toLowerCase();
+    const vocalArray = ["a", "e", "i", "o", "u"];
 
+    let counter = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (vocalArray.includes(string[i])) {
+            counter++;
+        }
+    } return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(counterVocal(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
